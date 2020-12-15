7d6a84820bf221e5594840761d780727dda3c947
Nicolas Notararigo <nicolas.notararigo@ynov.com>
2020-12-15 12:49:19 +0100

    Revert "Feat/reviews"

--- src/components/views/Lists/Reviews.js
+++ src/components/views/Lists/Reviews.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Table} from "react-bootstrap";
+import routeAPI from "../../../tools/routeAPI";
 
 class Reviews extends Component {
 
@@ -7,14 +8,28 @@ class Reviews extends Component {
         super(props);
         this.state = {
             page: "reviews",
-            tokenACP: '',
+            tokenACP: 'token',
+            dataType: this.props.location.pathname.substr(6),
             isLoading: false,
-            data: this.props.data,
+            data: [],
             activePage: '1',
         };
     }
 
+    async componentDidMount() {
+        await fetch(routeAPI + this.state.dataType+'/', {
+            method: "GET"
+            // headers: { 'Authorization': this.state.tokenACP },
+        }).then(response => response.json())
+            .then(json => {
+                if(json){
+                    this.setState({ data: json, apiLoaded: true });
+                }
+            });
+    }
+
     render() {
+        console.log(this.state)
         return (
             <div>
                 <Table striped bordered hover variant="dark" >
@@ -22,22 +37,21 @@ class Reviews extends Component {
                     <tr>
                         <th>#</th>
                         <th>Id</th>
+                        <th>Author Id</th>
+                        <th>Created At</th>
                         <th>Content</th>
-                        <th>créé le </th>
-                        <th>par</th>
-                        <th>note</th>
+                        <th>Note</th>
+                        <th>Actions</th>
                     </tr>
                     </thead>
+                    <h1>PANCAKE</h1>
                     <tbody>
                     {this.state.data.slice(this.props.startRange, this.props.endRange).map( (review, index) => {
+                        console.log(review);
                         return (
                             <tr key={index}>
                                 <td>{index+1}</td>
-                                <td>{review.id}</td>
-                                <td>{review.content}</td>
-                                <td>{Date(review.created_at)}</td>
-                                <td>{review.author_id}</td>
-                                <td>{review.note}</td>
+                                <td>{review.uid}</td>
                             </tr>
                         )
                     })}
